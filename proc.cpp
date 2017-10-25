633f50e0f3a1df023f985505e16ae5aff94c026e
popolee616 <x824li@uwaterloo.ca>
2017-10-25 17:03:43 -0400

    test pipe and fork between 2 processes

---
+++ proc.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <iostream>
+#include <sys/types.h>
+
+int main() {
+	int BtoC[2];
+	
+	pipe(BtoC);
+	
+	pid_t child_pid;
+	child_pid = fork();
+	
+	if (child_pid == 0) {
+		dup2(BtoC[1], STDOUT_FILENO);
+		close(BtoC[0]);
+		close(BtoC[1]);
+				
+		Input input;
+		input.get_input();
+		
+	
+	else {
+		dup2(BtoC[0], STDIN_FILENO);
+		close(BtoC[1]);
+		close(BtoC[0]);
+		
+		while (!std::cin.eof()) {
+			std::string line;
+			std::getline(std::cin, line);
+			if (line.size () > 0)
+				std::cout << line << std::endl;
+		}
+	}
+	
+	return 0;
+	
+}
